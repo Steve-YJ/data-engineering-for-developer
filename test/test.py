6840f7a4a225c1ad3352bb3e0171c29d1ae7c754
Steve-YJ <leeyoungjeon0511@gmail.com>
2024-09-10 17:43:19 +0900

    test commit

--- test/test.py
+++ test/test.py
@@ -4,3 +4,9 @@ print("Hello, World!")
 print("Test, Commit")
 
 # test-edit
+# edit user
+# cd /path/to/your/repo  # 리포지토리로 이동
+# git config user.name "Your Personal Name"
+# git config user.email "your_personal_email@example.com"
+
+
